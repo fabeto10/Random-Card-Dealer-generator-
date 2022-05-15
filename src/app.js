746bd5ff92ad3c8f8af4256a26969698e4a36981
fabeto10 <f0414475@gmail.com>
2022-05-15 23:35:08 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#boton").onclick = () => {
+    document.querySelector(".card").classList.add(generateRandomSuit());
+    document.querySelector(".card").innerHTML = generateRandomNumber();
+  };
+  setInterval(() => {
+    document.querySelector(".card").classList.add(generateRandomSuit());
+    document.querySelector(".card").innerHTML = generateRandomNumber();
+  }, 10000);
+};
+//input en proceso....
+var input = document.querySelector("input");
+input.addEventListener("input", resizeInput);
+resizeInput.call(input);
+
+function resizeInput() {
+  this.style.width = this.value.length + "ch";
+}
+//Es un poco lento al cargar pero si lo hace, por lo menos para mi :)
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suits = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suits.length);
+  return suits[indexSuit];
 };
